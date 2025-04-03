ae32cdd46d819c8680e004f53cf2b5b407bffbc8
danielpuliche <danielpuliche@unicauca.edu.co>
2025-04-03 01:11:10 -0500

    Añadido archivo de configuración con costos y confiabilidades por tipo de nodo

---
+++ config.py
@@ -0,0 +1,15 @@
+# Costos por tipo de nodo (Low, Medium, High)
+COST_BY_NODE_TYPE = {
+    0: 5,  # Low Cost
+    1: 12,  # Medium Cost
+    2: 30   # High Cost
+}
+
+# Confiabilidad por tipo de nodo (Low, Medium, High)
+RELIABILITY_BY_NODE_TYPE = [0.6, 0.7, 0.8]
+
+# Costo de un enlace
+LINK_COST = 10
+
+# Número de valores equidistantes para confiabilidades requeridas
+NUM_EQUIDISTANT_VALUES = 10
