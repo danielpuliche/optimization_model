379f4ef18220372c7b8b14a24d2606037932fb51
danielpuliche <danielpuliche@unicauca.edu.co>
2025-04-05 20:14:08 -0500

    Update cost and reliability configurations for improved accuracy

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
+# Configuration file for the network reliability analysis
+
+# Select the evaluation year for the analysis
+EVALUATION_YEAR = True # 2025: True, 2030: False
+
 # Costos por tipo de nodo (Low, Medium, High)
 COST_BY_NODE_TYPE = {
-    0: 5,  # Low Cost
-    1: 12,  # Medium Cost
-    2: 30   # High Cost
+    0: 24.2 if EVALUATION_YEAR else 10.74,  # Low Cost
+    1: 91.82 if EVALUATION_YEAR else 40.74,  # Medium Cost
+    2: 227.06 if EVALUATION_YEAR else 100.75   # High Cost
 }
 
 # Confiabilidad por tipo de nodo (Low, Medium, High)
 RELIABILITY_BY_NODE_TYPE = [0.9, 0.95, 0.99]
 
 # Costo de un enlace
-LINK_COST = 10
+LINK_COST = 7.69 if EVALUATION_YEAR else 3.41
 
 # Número de valores equidistantes para confiabilidades requeridas
-NUM_EQUIDISTANT_VALUES = 200
+NUM_EQUIDISTANT_VALUES = 100
 
 # Confiabilidad máxima
-MAX_RELIABILITY = 0.9999999
+MAX_RELIABILITY = 0.9999
